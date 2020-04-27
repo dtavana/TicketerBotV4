b5420219f036f5ac2aa92f509bf20a36b1fbadd7
dtavana <datavana@gmail.com>
2020-04-26 23:57:00 -0400

    Fix for process.env types

---
+++ src/environment.d.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import TicketerBotConfig from "./bot/client/TicketerBotConfig";
+
+declare global {
+    namespace NodeJS {
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
+        interface ProcessEnv extends TicketerBotConfig {}
+    }
+}
+
+export {};
