8e2bcd9336683202b92df8eedb0d48b98975f847
dtavana <datavana@gmail.com>
2020-04-27 15:23:21 -0400

    Add ReadyListener

---
+++ src/bot/listeners/ReadyListener.ts
@@ -0,0 +1,24 @@
+import { Listener } from 'discord-akairo';
+import { MESSAGES } from '../../lib/constants';
+import { TOPICS, EVENTS } from '../../utils/logger';
+
+export default class ReadyListener extends Listener {
+    public constructor() {
+        super("ready", {
+            emitter: "client",
+            event: "ready"
+        });
+    }
+
+    public async exec() {
+        this.client.logger.info(MESSAGES.EVENTS.READY.LOG(this.client), {
+            topic: TOPICS.DISCORD,
+            event: EVENTS.READY
+        });
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        this.client.user!.setActivity(
+            MESSAGES.EVENTS.READY.ACTIVITY(this.client.guilds.cache.size),
+            { type: "WATCHING" }
+        );
+    }
+}
