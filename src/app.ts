100ffe6d9bc3311a92613e8e34505e2a54e7eccf
dtavana <datavana@gmail.com>
2020-05-01 21:14:30 -0400

    Add logging on emitted errors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,26 @@
 import loadEnv from "./lib/loadEnv";
 import TicketerBot from "./bot/client/TicketerBot";
+import { TOPICS, EVENTS } from "./utils/logger";
 
 loadEnv("environment.yml");
-new TicketerBot().start().then();
+
+const client = new TicketerBot();
+
+client
+    .on("error", (err) =>
+        client.logger.error(err.message, {
+            topic: TOPICS.DISCORD,
+            event: EVENTS.ERROR
+        })
+    )
+    .on("shardError", (err, id) =>
+        client.logger.error(err.message, {
+            topic: TOPICS.DISCORD,
+            event: `SHARD ${id} ERROR`
+        })
+    )
+    .on("warn", (info) =>
+        client.logger.warn(info, { topic: TOPICS.DISCORD, event: EVENTS.WARN })
+    );
+
+client.start().then();
