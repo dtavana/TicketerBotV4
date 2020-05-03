15b9337380f671188662797496d62e4167e4a620
dtavana <datavana@gmail.com>
2020-05-03 18:18:18 -0400

    Add SettingsInhibitor

---
+++ src/bot/inhibitors/SettingsInhibitor.ts
@@ -0,0 +1,27 @@
+import { Inhibitor } from "discord-akairo";
+import { Message } from "discord.js";
+import { COMMAND_CATEGORIES, SETTINGS_PERMISSION } from "../../lib/constants";
+import { Command } from "discord-akairo";
+
+export default class SettingsInhibitor extends Inhibitor {
+    public constructor() {
+        super("settings", {
+            reason: "settings"
+        });
+    }
+
+    public exec(message: Message, command: Command) {
+        if (!message.guild) return false;
+        console.log(2);
+        if (command.categoryID !== COMMAND_CATEGORIES.CONFIG) {
+            return false;
+        }
+        console.log(2);
+        const hasPermission = message.member?.hasPermission(
+            SETTINGS_PERMISSION,
+            { checkAdmin: true, checkOwner: true }
+        );
+        console.log(2);
+        return !hasPermission;
+    }
+}
