ef8f9011415dca6322128f18367badb879320c0e
dtavana <datavana@gmail.com>
2020-05-03 18:17:37 -0400

    Add AdminRoleCommand

---
+++ src/bot/commands/config/AdminRoleCommand.ts
@@ -0,0 +1,51 @@
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+import {
+    COMMAND_CATEGORIES,
+    COMMAND_DESCRIPTIONS,
+    SETTINGS,
+    EMBEDS,
+    COMMAND_NAMES,
+    MESSAGES
+} from "../../../lib/constants";
+import { Role } from "discord.js";
+
+export default class AdminRoleCommand extends Command {
+    public constructor() {
+        super(COMMAND_NAMES.CONFIG.ADMINROLE, {
+            aliases: [COMMAND_NAMES.CONFIG.ADMINROLE, "set-admin-role"],
+            category: COMMAND_CATEGORIES.CONFIG,
+            description: COMMAND_DESCRIPTIONS.CONFIG.ADMINROLE,
+            args: [
+                {
+                    id: "target",
+                    type: "role"
+                }
+            ]
+        });
+    }
+
+    public async exec(message: Message, { target }: { target: Role }) {
+        let adminRole:
+            | string
+            | Role
+            | undefined = await this.client.settings.set(
+            message.guild,
+            SETTINGS.ADMINROLE,
+            target.id
+        );
+        if (adminRole === undefined) {
+            adminRole = MESSAGES.COMMANDS.CONFIG.NOTSET;
+        } else {
+            adminRole = message.guild?.roles.cache.get(adminRole);
+            if (!adminRole) {
+                adminRole = MESSAGES.COMMANDS.CONFIG.NOTSET;
+            }
+        }
+        await message.channel.send(
+            EMBEDS.SUCCESS().setDescription(
+                MESSAGES.COMMANDS.CONFIG.ADMINROLE(adminRole, target)
+            )
+        );
+    }
+}
