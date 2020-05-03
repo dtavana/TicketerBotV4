8f55983d131b113ddf8e7d9bf86b5df69a5a2849
dtavana <datavana@gmail.com>
2020-05-03 18:21:33 -0400

    Add constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,13 @@
 import { MessageEmbed } from "discord.js";
 import { AkairoClient } from "discord-akairo";
+import { User } from "discord.js";
+import { Role } from "discord.js";
 
 export const SETTINGS = {
     GUILDID: "guildId",
-    PREFIX: "prefix"
+    PREFIX: "prefix",
+    BLACKLIST: "blacklist",
+    ADMINROLE: "adminRole"
 };
 
 export const EMBEDS = {
@@ -20,8 +24,39 @@ export const EMBEDS = {
             .setTimestamp();
     }
 };
+export const COMMAND_NAMES = {
+    MODERATION: {
+        BLACKLIST: "blacklist",
+        UNBLACKLIST: "unblacklist"
+    },
+    CONFIG: {
+        SETUP: "setup",
+        ADMINROLE: "adminrole"
+    }
+};
 
 export const MESSAGES = {
+    COMMANDS: {
+        MODERATION: {
+            BLACKLIST: {
+                INVALID: (target: User) =>
+                    `${target.toString()} is already blacklisted in this server`,
+                SUCCESS: (target: User) =>
+                    `${target.toString()} has been added to the blacklist`
+            },
+            UNBLACKLIST: {
+                INVALID: (target: User) =>
+                    `${target.toString()} is not blacklisted in this server`,
+                SUCCESS: (target: User) =>
+                    `${target.toString()} has been removed from the blacklist`
+            }
+        },
+        CONFIG: {
+            NOTSET: "Not set",
+            ADMINROLE: (old: Role | string, target: Role) =>
+                `Old Admin Role: ${old.toString()}\nNew Admin Role: ${target.toString()}`
+        }
+    },
     COMMAND_HANDLER: {
         PROMPT: {
             MODIFY_START: (str: string) =>
@@ -49,11 +84,27 @@ export const MESSAGES = {
     },
     SETTINGS_MANAGER: {
         LOADED: "Settings manager has been loaded!"
-    },
-    COMMANDS: {}
+    }
 };
 
-export const COMMAND_CATEGORIES = {};
+export const SETTINGS_PERMISSION = "MANAGE_GUILD";
+
+export const COMMAND_CATEGORIES = {
+    MODERATION: "moderation",
+    CONFIG: "config"
+};
+
+export const COMMAND_DESCRIPTIONS = {
+    MODERATION: {
+        BLACKLIST:
+            "Use this command to blacklist a user from creating tickets in this server",
+        UNBLACKLIST:
+            "Use this command to remove a user from this servers blacklist"
+    },
+    CONFIG: {
+        ADMINROLE: "Use this command to set the administrator role"
+    }
+};
 
 export const ROLE_IDS = {};
 
@@ -62,14 +113,4 @@ export const CHANNEL_IDS = {};
 export const CLIENT_OPTIONS = {
     DEFAULT_PREFIX: "-",
     OWNERS: ["112762841173368832"]
-};
-
-export const PROMETHEUS = {
-    MESSAGE_COUNTER: "ticketer_messages_total",
-    COMMAND_COUNTER: "ticketer_commands_total",
-
-    HELP: {
-        MESSAGE_COUNTER: "Total number of messages Ticketer has seen.",
-        COMMAND_COUNTER: "Total number of commands used."
-    }
 };
