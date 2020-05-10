40ddb54d40b94aaf34c7d023756ab97e070de606
dtavana <datavana@gmail.com>
2020-05-10 02:40:39 -0400

    Add DebugListener

---
+++ src/bot/listeners/Client/DebugListener.ts
@@ -0,0 +1,19 @@
+import { Listener } from "discord-akairo";
+import { EVENTS, TOPICS } from "../../../utils/logger";
+
+export default class DebugListener extends Listener {
+    public constructor() {
+        super("debug", {
+            emitter: "client",
+            event: "debug",
+            category: "client"
+        });
+    }
+
+    public exec(event: any) {
+        this.client.logger.debug(event, {
+            topic: TOPICS.DISCORD,
+            event: EVENTS.DEBUG
+        });
+    }
+}
