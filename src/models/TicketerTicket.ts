0dc6bf232bce238f2e2685a44a0d712a67f32d57
dtavana <datavana@gmail.com>
2020-05-16 00:03:53 -0400

    Remove unused import

--- src/models/TicketerTicket.ts
+++ src/models/TicketerTicket.ts
@@ -1,4 +1,4 @@
-import { prop, getModelForClass, Ref, getName } from "@typegoose/typegoose";
+import { prop, getModelForClass, Ref } from "@typegoose/typegoose";
 import { TicketerChannel } from "./TicketerChannel";
 
 export class TicketerTicket {
