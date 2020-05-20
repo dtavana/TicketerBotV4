1c61276f81c38c593093ceb2850e393c97230cbc
dtavana <datavana@gmail.com>
2020-05-20 13:28:40 -0400

    Add Listeners to initialize guild settings

---
+++ src/bot/listeners/Client/GuildCreateListener.ts
@@ -0,0 +1,21 @@
+import { Listener } from "discord-akairo";
+import { MESSAGES } from "../../../lib/constants";
+import { TOPICS, EVENTS } from "../../../utils/logger";
+import { Guild } from "discord.js";
+
+export default class GuildCreateListener extends Listener {
+    public constructor() {
+        super("guildCreate", {
+            emitter: "client",
+            event: "guildCreate"
+        });
+    }
+
+    public async exec(guild: Guild) {
+        await this.client.settings.initGuild(guild);
+        this.client.logger.info(MESSAGES.EVENTS.GUILD_CREATE.LOG(guild), {
+            topic: TOPICS.DISCORD,
+            event: EVENTS.GUILD_CREATE
+        });
+    }
+}
---
+++ src/bot/listeners/Client/GuildDeleteListener.ts
@@ -0,0 +1,21 @@
+import { Listener } from "discord-akairo";
+import { MESSAGES } from "../../../lib/constants";
+import { TOPICS, EVENTS } from "../../../utils/logger";
+import { Guild } from "discord.js";
+
+export default class GuildCreateListener extends Listener {
+    public constructor() {
+        super("guildDelete", {
+            emitter: "client",
+            event: "guildCreate"
+        });
+    }
+
+    public async exec(guild: Guild) {
+        await this.client.settings.clear(guild);
+        this.client.logger.info(MESSAGES.EVENTS.GUILD_DELETE.LOG(guild), {
+            topic: TOPICS.DISCORD,
+            event: EVENTS.GUILD_DELETE
+        });
+    }
+}
