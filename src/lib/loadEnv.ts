9dbce34c9345b888d2a6223d2fa24073b99fd211
dtavana <datavana@gmail.com>
2020-05-25 17:18:59 -0400

    Remove no-loops exception

--- src/lib/loadEnv.ts
+++ src/lib/loadEnv.ts
@@ -3,7 +3,6 @@ import { safeLoad } from "js-yaml";
 import TicketerBotConfig from "../bot/client/TicketerBotConfig";
 
 const saveToEnv = (object: TicketerBotConfig) => {
-    // eslint-disable-next-line no-loops/no-loops
     for (const [key, value] of Object.entries(object)) {
         process.env[key] = value;
     }
